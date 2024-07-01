670a08dac74c9743fbe16bc9e893a2be3258a21c
MahabirGupta <mahabirg81@gmail.com>
2024-07-01 17:41:56 +0800

    Add Error Component to our React App

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -9,6 +9,7 @@ export default function TodoApp() {
         <Route path='/' element={ <LoginComponent /> }></Route>
         <Route path='/login' element={ <LoginComponent /> }></Route>
         <Route path='/welcome' element={<WelcomeComponent /> }></Route>
+        <Route path='*' element={<ErrorComponent /> }></Route>
     </Routes>
     </BrowserRouter>
             {/* <LoginComponent />
@@ -54,7 +55,7 @@ function LoginComponent() {
     
     return (
         <div className="Login">
-           
+           <h1>Time to Login!</h1>
             <div className="LoginForm">
             
                 <div>
@@ -96,12 +97,24 @@ function LoginComponent() {
     // }
 }
 
-
-
 function WelcomeComponent() {
     return (
-        <div className="Welcome">
-            Welcome Component
+        <div className="WelcomeComponent">
+            <h1>Welcome in28minutes</h1>
+            <div>
+                Welcome Component
+            </div>
+        </div>
+    )
+}
+
+function ErrorComponent() {
+    return (
+        <div className="ErrorComponent">
+            <h1>We are working really hard!</h1>
+            <div>
+                Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.
+            </div>
         </div>
     )
 }
